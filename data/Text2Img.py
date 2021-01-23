e31c53990313e0cbc9941850ee8670689504e84f
eddie94 <myeddie@naver.com>
2021-01-23 20:15:04 +0900

    Image size changed

--- data/Text2Img.py
+++ data/Text2Img.py
@@ -9,20 +9,21 @@ from PIL import ImageFont, ImageDraw, Image
 # a function to draw training images
 def draw_image(textlist, save_path):
 
-    full_img = np.zeros((1, 200, 3), dtype=np.uint8)
+    full_img = np.zeros((0, 256, 3), dtype=np.uint8)
 
     for text in textlist:
 
-        img = np.zeros((60, 200, 3), dtype=np.uint8)
+        img = np.zeros((60, 256, 3), dtype=np.uint8)
         img_pil = Image.fromarray(img)
         draw = ImageDraw.Draw(img_pil)
         draw.text((30, 20), text, font=font, fill=(b, g, r))
 
+
         full_img = np.concatenate([full_img, img_pil], axis=0)
 
     # make sure that the images are in the same size
-    if len(textlist) < 3:
-        full_img = np.concatenate([full_img, np.zeros((60*(3-len(textlist)), 200, 3),)], axis=0)
+    if len(textlist) < args.n_text:
+        full_img = np.concatenate([full_img, np.zeros((60*(args.n_text-len(textlist)), 256, 3),)], axis=0)
 
     # make sure that the save path exists
     if not os.path.exists(save_path):
@@ -36,13 +37,10 @@ if __name__ == '__main__':
     parser.add_argument("--text_file_path", type=str, default="./")
     parser.add_argument("--text_file_name", type=str, default="sample_text.txt")
     parser.add_argument("--save_path", type=str, default="./images/")
-    parser.add_argument("--n_text", type=int, default=3)
+    parser.add_argument("--n_text", type=int, default=4)
 
     args = parser.parse_args()
-
     n_text = args.n_text
-
-    a = [i for i in range(0, 100)]
 
     with open(os.path.join(args.text_file_path, args.text_file_name), 'r', encoding="utf-8") as textFile:
         lines = textFile.read().split("\n")

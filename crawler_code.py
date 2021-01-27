e7e498cd88e959a926b582bee57b404c93452f69
honor-sky <keung903@naver.com>
2021-01-27 23:19:50 +0900

    Update crawler_code.py

--- crawler_code.py
+++ crawler_code.py
@@ -2,20 +2,24 @@ import requests
 from bs4 import BeautifulSoup
 import re
 
-url=''
+url='https://namu.wiki/w/%EC%84%B1%ED%83%84%EC%A0%88'
 resp = requests.get(url)
 
 if(resp.status_code==200): #정상 처리
     soup = BeautifulSoup(resp.text, 'html.parser') #parsing위해 BeautifulSoup 객체 생성
-    body = soup.find_all('body')
 
-    contents='' #태그 안의 value값 추출해 저장하는 문자열 변수
+    if 'namu.wiki' in url:
+        body = soup.find_all(class_='w')
+    else:
+        body = soup.find_all('body')
+
+    contents=''
     for i in body:
         contents += i.get_text()
 
-    new_contents = re.findall('[가-힣\n. ]', contents)  # 순수 한글,줄바꿈,띄어쓰기만 뽑아냄 #각 문자가 리스트에 저장
+    new_contents = re.findall('[가-힣\n. ]', contents)  # 순수 한글,줄바꿈,띄어쓰기만 뽑아냄
 
-    #시작:한글로 시작
+    #첫 한글 나오기 전 문자 제거
     for i in range(len(new_contents)):
         if(new_contents[i].isalpha()):
             break
@@ -31,7 +35,7 @@ if(resp.status_code==200): #정상 처리
                     new_contents[i + 1] = '\n'
 
 
-    #\n가 한번 나오고 다시 한글 나올때까지 모든 특수 문자 제거
+    #\n 나온 후 한글 나옴
     for i in range(len(new_contents)):
         if(new_contents[i] == '\n'):
             for k in range(len(new_contents)-i-1):
@@ -46,16 +50,13 @@ if(resp.status_code==200): #정상 처리
             if (new_contents[i + 1] == ' '):
                 new_contents[i] = ""
 
-    #마지막 줄바꿈 제거
+    #마지막 줄바꿈 제거 후 끝에 줄바꿈 넣어줌
     for i in range(len(new_contents)-1,0,-1):
         if(new_contents[i]=='\n'):
             new_contents[i]=''
         if(new_contents[i].isalpha()):
+            new_contents.append('\n')
             break
-
-    #끝:줄바꿈
-    new_contents.append('\n')
-
 
     #텍스트 파일에 저장
     f = open('SYR_crawling.txt', 'a')

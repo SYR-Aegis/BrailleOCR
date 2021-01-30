1b38410d59d892825cdfe397362e134a59c48d6e
honor-sky <keung903@naver.com>
2021-01-30 19:06:31 +0900

    Update Retouch_crawler.py

--- Retouch_crawler.py
+++ Retouch_crawler.py
@@ -5,17 +5,19 @@ new_f = open('new_crawling.txt','w')
 text = f.read()
 text = text.replace(' ','\n')
 text = text.replace('\n\n','\n')
-new_text = text.split('\n')
+new_text = text.split('\n') #리스트
+for i in range(len(new_text)):
+    new_f.write(new_text[i] + '\n')
 
 # 글자 딕셔너리 파일 생성
 data_dict = {}
-for idx in range(len(new_text)):
-    if new_text[idx] not in data_dict:
-        data_dict[new_text[idx]] = idx
-    new_f.write(new_text[idx] + '\n')
+for char in text:
+    if char not in data_dict:
+        if(char!= '\n'):
+            data_dict[char] = len(data_dict)
 
-data_f = open('dict_file.txt',w)
-data_f.write(data_dict)
+data_f = open('dict_file.txt','w')
+data_f.write(str(data_dict))
 
 f.close()
 new_f.close()

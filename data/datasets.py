03b38d51c19733259e0e1a1fcee24cd8d1942489
eddie94 <myeddie@naver.com>
2021-01-30 17:54:45 +0900

    dataset added

---
+++ data/datasets.py
@@ -0,0 +1,23 @@
+from torch.utils.data.dataset import Dataset
+
+
+class TLGAN_Dataset(Dataset):
+    def __init__(self):
+        pass
+
+    def __len__(self):
+        pass
+
+    def __getitem__(self, idx):
+        pass
+
+
+class CRNN_Dataset(Dataset):
+    def __init__(self):
+        pass
+
+    def __len__(self):
+        pass
+
+    def __getitem__(self, idx):
+        pass

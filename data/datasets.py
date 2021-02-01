731a5e9ad9f7fb50b69ed9b15e3b6887c6af4eaa
honor-sky <keung903@naver.com>
2021-02-01 23:08:22 +0900

    Update datasets.py

--- data/datasets.py
+++ data/datasets.py
@@ -1,6 +1,7 @@
 import cv2
 import numpy as np
-
+from PIL import Image
+import os
 from torch.utils.data.dataset import Dataset
 
 
@@ -20,11 +21,18 @@ class TLGAN_Dataset(Dataset):
 
 
 class CRNN_Dataset(Dataset):
-    def __init__(self):
-        pass aaaa
+    def __init__(self, path_to_csv="./CRNN.csv"):
+        self.img_file=[]
+
+        with open(path_to_csv, "r") as f:
+            for line in f.readlines():
+                self.img_file.append(line.strip().split(','))
 
     def __len__(self):
-        pass
+        return len(self.img_file)
 
     def __getitem__(self, idx):
-        pass
+        image = np.array(Image.open('./imgaes/',self.img_file[idx][0]))
+        label = self.img_file[idx][1:]
+
+        return image,label

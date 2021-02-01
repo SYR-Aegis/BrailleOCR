e1d3b492ca72b803b86ce7c4911cfd59b666b9ad
eddie94 <myeddie@naver.com>
2021-02-01 15:10:46 +0900

    need ground truth

--- data/datasets.py
+++ data/datasets.py
@@ -1,9 +1,16 @@
+import cv2
+import numpy as np
+
 from torch.utils.data.dataset import Dataset
 
 
 class TLGAN_Dataset(Dataset):
-    def __init__(self):
-        pass
+    def __init__(self, path_to_csv="./TLGAN.csv"):
+        imgs = np.zeros((0, 3, 256, 256))
+        labels = np.zeros((0))
+
+        with open(path_to_csv, 'r') as f:
+            pass
 
     def __len__(self):
         pass

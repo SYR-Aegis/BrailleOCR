1024aabef233556eeb8eab30c848d4e331bf2fd1
honor-sky <keung903@naver.com>
2021-02-01 17:08:50 +0900

    Update Retouch_crawler.py

--- Retouch_crawler.py
+++ Retouch_crawler.py
@@ -9,7 +9,7 @@ new_text = text.split('\n') #리스트
 for i in range(len(new_text)):
     new_f.write(new_text[i] + '\n')
 
-# 글자 딕셔너리 파일 생성
+# label 딕셔너리 파일 생성
 data_dict = {}
 for char in text:
     if char not in data_dict:
@@ -17,7 +17,8 @@ for char in text:
             data_dict[char] = len(data_dict)
 
 data_f = open('dict_file.txt','w')
-data_f.write(str(data_dict))
+data_f.write(str(data_dict).replace('\'','\"'))
+
 
 f.close()
 new_f.close()

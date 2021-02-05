49eaef4dd592bd45f122b5f6d77b370aa6eaa6b9
honor-sky <keung903@naver.com>
2021-02-05 17:16:17 +0900

    Create generate_dataset.py

---
+++ data/generate_dataset.py
@@ -0,0 +1,110 @@
+import cv2
+import argparse
+import numpy as np
+import os
+import sys
+
+from PIL import ImageFont, ImageDraw, Image
+from pathlib import Path
+
+# a function to draw training images
+def draw_image(textlist, TLGAN_save_path, CRNN_save_path):
+
+    # make sure that the save path exists
+    Path(TLGAN_save_path).mkdir(parents=True, exist_ok=True)
+    Path(CRNN_save_path).mkdir(parents=True, exist_ok=True)
+
+    full_img = np.zeros((0, 256, 3), dtype=np.uint8)
+    bounding_boxes = []
+    crnn_dataframe = {}
+
+    for text, i in zip(textlist, range(len(textlist))):
+
+        if len(text) <= 10:
+            img = np.zeros((60, 256, 3), dtype=np.uint8)
+            img_pil = Image.fromarray(img)
+            draw = ImageDraw.Draw(img_pil)
+            draw.text((30, 20), text, font=font, fill=(b, g, r))
+
+            #draw CRNN image
+            cv2.imwrite(os.path.join(CRNN_save_path, str(len(os.listdir(CRNN_save_path))) + ".jpg"), np.array(img_pil))
+            crnn_dataframe.update({str(len(os.listdir(CRNN_save_path))) + ".jpg": text})
+
+            full_img = np.concatenate([full_img, img_pil], axis=0)
+
+            xmin = 30
+            xmax = xmin + 20*len(text)
+            ymin = 20 + 60*i
+            ymax = ymin+20
+
+            bounding_boxes.append([xmin, xmax, ymin, ymax])
+        else:
+            full_img = np.concatenate([full_img, np.zeros((60, 256, 3))], axis=0)
+
+
+    # make sure that the images are in the same size
+    if len(textlist) < args.n_text:
+        full_img = np.concatenate([full_img, np.zeros((60*(args.n_text-len(textlist)), 256, 3),)], axis=0)
+
+    cv2.imwrite(os.path.join(TLGAN_save_path, str(len(os.listdir(TLGAN_save_path))) + ".jpg"), full_img)
+
+    return {str(len(os.listdir(TLGAN_save_path))) + ".jpg": bounding_boxes}, crnn_dataframe
+
+
+def writeCSV(TLGAN_dataframe, CRNN_dataframe, TLGAN_csv_filename="TLGAN.csv", CRNN_csv_filename="CRNN.csv"):
+
+    with open(TLGAN_csv_filename, "w") as f:
+        for filename in TLGAN_dataframe.keys():
+            f.write(filename)
+            for bb in TLGAN_dataframe[filename]:
+                for axis in bb:
+                    f.write(","+str(axis))
+            f.write("\n")
+
+    with open(CRNN_csv_filename, 'w', encoding="utf-8") as f:
+        for filename in CRNN_dataframe.keys():
+            f.write(filename)
+            for label in CRNN_dataframe[filename]:
+                f.write(","+str(label))
+            f.write("\n")
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description="Convert text to image file")
+    parser.add_argument("--text_file_path", type=str, default="./")
+    parser.add_argument("--text_file_name", type=str, default="sample_text.txt")
+    parser.add_argument("--TLGAN_save_path", type=str, default="./images/TLGAN")
+    parser.add_argument("--CRNN_save_path", type=str, default="./images/CRNN")
+    parser.add_argument("--n_text", type=int, default=4)
+
+    args = parser.parse_args()
+    n_text = args.n_text
+
+    tlgan_csv = {}
+    crnn_csv = {}
+
+    with open(os.path.join(args.text_file_path, args.text_file_name), 'r', encoding="utf-8") as textFile:
+        lines = textFile.read().split("\n")
+
+        b, g, r = 255, 255, 255
+        fontpath = "fonts/H2GTRM.TTF"
+        font = ImageFont.truetype(fontpath, 20)
+
+        if len(lines) <= n_text:
+            tlgan, crnn = draw_image(lines, args.TLGAN_save_path, args.CRNN_save_path)
+            tlgan_csv.update(tlgan)
+            crnn_csv.update(crnn)
+
+        else:
+            for i in range(0, len(lines), n_text):
+
+                if i+n_text >= len(lines):
+                    tlgan, crnn = draw_image(lines[i:], args.TLGAN_save_path, args.CRNN_save_path)
+                    tlgan_csv.update(tlgan)
+                    crnn_csv.update(crnn)
+
+                else:
+                    tlgan, crnn = draw_image(lines[i:i+n_text], args.TLGAN_save_path, args.CRNN_save_path)
+                    tlgan_csv.update(tlgan)
+                    crnn_csv.update(crnn)
+    writeCSV(tlgan_csv, crnn_csv)

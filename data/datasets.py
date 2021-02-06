8c10254724203738ef6e808035a2da336ed5ca24
honor-sky <keung903@naver.com>
2021-02-06 19:22:53 +0900

    Update datasets.py
    CRNN_dataset class 추가

--- data/datasets.py
+++ data/datasets.py
@@ -2,6 +2,8 @@ import cv2
 import numpy as np
 import os
 import torch
+from PIL import Image
+import json
 
 from torch.utils.data.dataset import Dataset
 
@@ -49,11 +51,28 @@ class TLGAN_Dataset(Dataset):
 
 
 class CRNN_Dataset(Dataset):
-    def __init__(self):
-        pass
+    def __init__(self, path_to_csv = "./CRNN.csv", dict_dir="dict_file.txt", root = './images/CRNN', ):
+        self.img_file=[]
+        self.label = []
+        self.root = root
+
+        with open(path_to_csv, "r",encoding = "utf-8") as f:
+            for line in f.readlines():
+                self.img_file.append(line.strip().split(','))
+
+        with open(dict_dir, "r",encoding = "utf-8") as fil:
+            dict = fil.read()
+
+        self.label_dict = json.loads(dict)
 
     def __len__(self):
-        pass
+        return len(self.img_file)
 
     def __getitem__(self, idx):
-        pass
+        image = np.array(Image.open(os.path.join(self.root,self.img_file[idx][0]))).astype(np.int32)
+        image = torch.tensor(image/255,dtype=torch.float32)
+
+        for key in self.img_file[idx][1:]:
+            self.label.append(self.label_dict[key])
+
+        return image, self.label
